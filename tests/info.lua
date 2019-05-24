2c3573b983b6d53a9a3841f39285b8fbdc90eb88
Christopher Moore <christopher.e.moore@gmail.com>
2019-05-24 11:45:34 +0800

    updated info

--- tests/info.lua
+++ tests/info.lua
@@ -1,2 +1,21 @@
 #!/usr/bin/env luajit
-require 'cl'{verbose=true}
+
+local table = require 'ext.table'
+local platforms = require 'cl.platform'.getAll()
+for i,platform in ipairs(platforms) do
+	print()
+	print('platform '..i)
+	platform:printInfo()
+
+	local devices = platform:getDevices()
+	for i,device in ipairs(devices) do
+		print()
+		print(' device '..i)
+		device:printInfo()
+		
+		local context = require 'cl.context'(table({platform=platform, device=device}))
+		print()
+		print'  context'
+		context:printInfo()
+	end
+end
--- tests/reduce.lua
+++ tests/reduce.lua
@@ -3,6 +3,7 @@ local range = require 'ext.range'
 local table = require 'ext.table'
 
 -- TODO env.size optional?  for no env.base?  but env:kernel needs env.base ...
+-- TODO easy way to pick platform or device
 local env = require 'cl.obj.env'()
 
 local maxWorkGroupSize = tonumber(env.device:getInfo'CL_DEVICE_MAX_WORK_GROUP_SIZE')
