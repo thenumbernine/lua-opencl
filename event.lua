13d0c659ff98f8213af36320233a460b55d6b95b
thenumbernine <christopher.e.moore@gmail.com>
2022-01-01 18:30:57 +0600

    added some comments

--- event.lua
+++ event.lua
@@ -30,10 +30,12 @@ end
 Event.getInfo = Event:makeGetter{
 	getter = cl.clGetEventInfo,
 	vars = {
+		-- 1.0:
 		{name='CL_EVENT_COMMAND_QUEUE', type='cl_command_queue'},
 		{name='CL_EVENT_COMMAND_TYPE', type='cl_command_type'},
 		{name='CL_EVENT_REFERENCE_COUNT', type='cl_uint'},
 		{name='CL_EVENT_COMMAND_EXECUTION_STATUS', type='cl_int'},
+		-- 1.1
 		{name='CL_EVENT_CONTEXT', type='cl_context'},
 	},
 }
@@ -41,11 +43,13 @@ Event.getInfo = Event:makeGetter{
 Event.getProfilingInfo = Event:makeGetter{
 	getter = cl.clGetEventProfilingInfo,
 	vars = {
+		-- 1.0
 		{name='CL_PROFILING_COMMAND_QUEUED', type='cl_ulong'},
 		{name='CL_PROFILING_COMMAND_SUBMIT', type='cl_ulong'},
 		{name='CL_PROFILING_COMMAND_START', type='cl_ulong'},
 		{name='CL_PROFILING_COMMAND_END', type='cl_ulong'},
-		{name='CL_PROFILING_COMMAND_COMPLETE', type=''},
+		-- 2.0
+		{name='CL_PROFILING_COMMAND_COMPLETE', type='cl_ulong'},
 	},
 }
 
