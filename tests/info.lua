996c71456c1576d9d4a83f69048fc7c28cb1e048
Christopher Moore <christopher.e.moore@gmail.com>
2024-04-03 19:12:04 +0800

    fixed up info

--- tests/info.lua
+++ tests/info.lua
@@ -1,21 +1,32 @@
 #!/usr/bin/env luajit
 
-local table = require 'ext.table'
+local function report(f)
+	xpcall(f, function(err)
+		io.stderr:write(err..'\n'..debug.traceback()..'\n')
+	end)
+end
+
 local platforms = require 'cl.platform'.getAll()
 for i,platform in ipairs(platforms) do
 	print()
 	print('platform '..i)
-	platform:printInfo()
+	report(function()
+		platform:printInfo()
+	end)
 
-	local devices = platform:getDevices()
-	for j,device in ipairs(devices) do
+	report(function()
+		local devices = platform:getDevices()
+		for j,device in ipairs(devices) do
+			print()
+			print('-device '..j)
+			report(function()
+				device:printInfo()
+			end)
+		end
+
+		local context = require 'cl.context'{platform=platform, devices=devices}
 		print()
-		print('-device '..j)
-		device:printInfo()
-	end
-
-	local context = require 'cl.context'(table({platform=platform, devices=devices}))
-	print()
-	print'--context'
-	context:printInfo()
+		print'--context'
+		context:printInfo()
+	end)
 end
